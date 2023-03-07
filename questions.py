56db203c23723bff678da6a7e5fa895b5f4cc8fe
benjackson3811 <benjackson3811@googlemail.com>
2023-03-07 15:31:17 +0000

    Adding quiz questions, alternatives and answers for land animals

---
+++ questions.py
@@ -0,0 +1,56 @@
+"""
+file contains the questions and alternative multiple choice questions.
+The answer is always the 1st answer.
+To stop the user realising this - the alternatives are sorted.
+"""
+
+sea_questions = {
+    "What percentage of the Earth's surface is covered by Oceans?: ":
+    ["A. 70%", "B. 50%", "C. 40%", "D. 20%"],
+    "What is the percentage of the planet's oceans that have been explored? ": 
+    ["A. 5%", "B. 25%", "C. 50%", "D. 75%"],
+    "Which one the following islands is not in the Atlantic Ocean? ": 
+    ["A. Madagascar", "B. Azores", "C, The Bahanmas", "D, Canarary Islands"],
+    "Which of the following oceans is the smallest and the shallowest? ":
+    ["A, Arctic", "B. Pacific", "C. Indian", "D. Atlantic"],
+    "What is the name of the deepest known trench on the planet in the sea? ":
+    ["A. Mariana Trench", "B. The Trench", "C. A Trench", "D.Izu-Ogas Trench"],
+}
+
+cape_town_history = {
+    "What other name is Cape Town also known as? ":
+    ["A. Mother City", "B. Father City", "C. Golden City", "D. Ancient City"],
+    "In which province is the city located? ":
+    ["A. Western Cape", "B. Eastern Cape", "C. North Cape", "D. South Cape"],
+    "What year was the city founded?: ":
+    ["A. 1652", "B. 1624", "C. 1691", "D.1624"],
+    "What language is majorly used in the city? ":
+    ["A. English", "B. Xhorsa", "C. Arikaans", "D. Bandu"],
+    "Which of these were instrumental in the creation of the city? ":
+    ["A. Dutch East India Company", "B. HSBC", "C. PEPSI", "D. The Company"],
+    "At which hospital was the worlds first ever heart trasnplant performed? ":
+    ["A. Groote Schuur", "B. Tygerberg", "C. Great Almond Street", "D. A&E"],
+    "What is the name of the ocean that surrounds Cape Town? ":
+    ["A. Atlanic", "B. Indian", "C. Artic", "D. Pacfic"],
+    "What Alcohic beverage is the Cape famous for producing? ":
+    ["A. Wine", "B. Sake", "C. Whiskey", "D. Vodka"],
+}
+
+land_animals_questions = {
+    "Which country has the most sheep in the world? ":
+    ["A. China ", "B. Wales","C. India","D. Ireland"],
+    "What is the worlds fastest land animal? ":
+    ["A. Cheetah", "B. Carcal","C, leopard","D. Lion"],
+    "What is the largest feline cat in Africa?":
+    ["A. Lion", "B. Cheetah","C, Carcal","D. Tom"],
+    "Which creature spends most of its time alone, rather than in a group?":
+    ["A. Leopard", "B. Carcal","C, Cheetah","D. Lion"],
+    "":
+    ["A. ", "B. ","C, ","D. "],
+    "":
+    ["A. ", "B. ","C, ","D. "],
+    "":
+    ["A. ", "B. ","C, ","D. "],
+    "":
+    ["A. ", "B. ","C, ","D. "],
+}
