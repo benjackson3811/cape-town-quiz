6a544f19b472e7b978ae68bbd9f03d00d3ac8f71
benjackson3811 <benjackson3811@googlemail.com>
2023-03-08 07:05:36 +0000

    Adding final set of questions to questions.py

--- questions.py
+++ questions.py
@@ -7,14 +7,24 @@ To stop the user realising this - the alternatives are sorted.
 sea_questions = {
     "What percentage of the Earth's surface is covered by Oceans?: ":
     ["A. 70%", "B. 50%", "C. 40%", "D. 20%"],
-    "What is the percentage of the planet's oceans that have been explored? ": 
+    "What is the percentage of the planet's oceans that have been explored? ":
     ["A. 5%", "B. 25%", "C. 50%", "D. 75%"],
-    "Which one the following islands is not in the Atlantic Ocean? ": 
+    "Which one the following islands is not in the Atlantic Ocean? ":
     ["A. Madagascar", "B. Azores", "C, The Bahanmas", "D, Canarary Islands"],
     "Which of the following oceans is the smallest and the shallowest? ":
     ["A, Arctic", "B. Pacific", "C. Indian", "D. Atlantic"],
     "What is the name of the deepest known trench on the planet in the sea? ":
     ["A. Mariana Trench", "B. The Trench", "C. A Trench", "D.Izu-Ogas Trench"],
+    "How many Oceans does the equator cross? ":
+    ["A. 3", "B. 2", "C. 5", "D. 4"],
+    "Approx ____ of the Earth's surface is covered by Ocean? ":
+    ["A. 71%", "B. 56%", "C. 45%", "D. 40%"],
+    "The underground Ring of Fire zone is along the floor of which ocean? ":
+    ["A. Pacific", "B. Atlantic", "C. Southern", "D. Arctic"],
+    "Which ocean is frozen for much of the year? ":
+    ["A. Arctic", "B. Atlantic", "C. Indian", "D. Pacific"],
+    "Which ocean lies between Australia and Africa? ":
+    ["A. Indian", "B. Arctic", "C. Atlantic", "D. Pacific"],
 }
 
 cape_town_history = {
@@ -34,23 +44,29 @@ cape_town_history = {
     ["A. Atlanic", "B. Indian", "C. Artic", "D. Pacfic"],
     "What Alcohic beverage is the Cape famous for producing? ":
     ["A. Wine", "B. Sake", "C. Whiskey", "D. Vodka"],
+    "In what part of South Africa is Cape Town Located?? ":
+    ["A. South", "B. East", "C. North", "D. West"],
+    "What is mined at Kimberley in South Africa?":
+    ["A. Diamonds", "B. Gold", "C. Silver", "D. Wine"],
 }
 
 land_animals_questions = {
     "Which country has the most sheep in the world? ":
-    ["A. China ", "B. Wales","C. India","D. Ireland"],
+    ["A. China", "B. Wales", "C. India", "D. Ireland"],
     "What is the worlds fastest land animal? ":
-    ["A. Cheetah", "B. Carcal","C, leopard","D. Lion"],
+    ["A. Cheetah", "B. Carcal", "C, leopard", "D. Lion"],
     "What is the largest feline cat in Africa?":
-    ["A. Lion", "B. Cheetah","C, Carcal","D. Tom"],
+    ["A. Lion", "B. Cheetah", "C, Carcal", "D. Tom"],
     "Which creature spends most of its time alone, rather than in a group?":
-    ["A. Leopard", "B. Carcal","C, Cheetah","D. Lion"],
-    "":
-    ["A. ", "B. ","C, ","D. "],
-    "":
-    ["A. ", "B. ","C, ","D. "],
-    "":
-    ["A. ", "B. ","C, ","D. "],
-    "":
-    ["A. ", "B. ","C, ","D. "],
-}
+    ["A. Leopard", "B. Carcal", "C, Cheetah", "D. Lion"],
+    "What is the closest living relative of the Hippo? ":
+    ["A. Whale", "B. Warthog", "C, Rhino", "D. Elephant"],
+    "How wide can a hippo stretch it's jaws? ":
+    ["A. 150 degrees", "B. 120 degrees", "C, 90 degrees", "D. 65 degrees"],
+    "How many legs does a butterfly have? ":
+    ["A. 6", "B. 8", "C. 2", "D. 4"],
+    "Which of the following is not a true snake? ":
+    ["A. Glass snake", "B. Tree Snake", "C. Blind Snake", "D. Sea Snake"],
+    "What is Rhino Horn made of? ":
+    ["A. Kerotin", "B. Bones", "C. Collagen", "D. Fibroin"],
+    }
